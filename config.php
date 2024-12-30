dfefba2dcdd1fabe3d7760c58239f3b34cafea81
Erick Ochieng <erickochieng830@gmail.com>
2024-12-30 22:43:47 +0300

    php ecommerce website

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$currency = '₹';
+$db_username = 'kelvin';
+$db_password = 'Demon@56';
+$db_name = 'bolt';
+$db_host = '127.0.0.1:3307';
+$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
+?>
